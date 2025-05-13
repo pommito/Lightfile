1a570fbabed850e7f96cb7aa4d06bbe912748e5d
Victor Lebecq <victor.lebecq.pro@gmail.com>
2025-05-13 11:00:25 +0200

    (feat) : add shadcn to the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import { buttonVariants } from '@/components/ui/button';
 import Link from 'next/link';
 
 export default function Page() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <h1>Home</h1>
-      <Link href="/about">About</Link>
+      <Link className={buttonVariants({ variant: 'default' })} href="/about">
+        About
+      </Link>
     </div>
   );
 }
